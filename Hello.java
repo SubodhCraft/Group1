6a97ebdfd4f162cb8a485d5aa35aa4270c19c589
SubodhCraft <nepaliboiz123@gmail.com>
2025-04-11 22:56:10 +0545

    all added

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("A well-formed Java program has a main method with { and } braces.\n\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character. (But we type \\\" instead!)\nWhat is the difference between a ' and a \"? Or between a \" and a \\\"? \n\nOne is what we see when we're typing our program. The other is what appears on the \"console.\"");
     }
 }
 
