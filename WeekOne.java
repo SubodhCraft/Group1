70d5337ad1ef9951ca5bd93f04cc5dfbf2895b68
SubodhCraft <nepaliboiz123@gmail.com>
2025-04-11 12:01:46 +0545

    All new files added

---
+++ WeekOne.java
@@ -0,0 +1,17 @@
+public class WeekOne {
+    public static void main(String[] args) {
+        String var1="Tony\tStark";
+        System.out.println(var1);
+        String var2="If you can dream it \n you can do it";
+        System.out.println(var2);
+        String var3="And\\Or";
+        System.out.println(var3);
+        String var4="Carriage\rWorld";
+        System.out.println(var4);
+        String var5="Wall Street's";
+        System.out.println(var5);
+        String var6="Avengers 'Iron Man' Tony Stark";
+        System.out.println(var6);
+    }
+    
+}
---
+++ clg.java
@@ -0,0 +1,10 @@
+public class clg {
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello,\nWorld!"
+        );
+        // Hello this is my first java code. In exam comments do carry certain marks
+        /* This is multi lined comment which is created by Shift+alt+A*/
+    }
+    
+}
